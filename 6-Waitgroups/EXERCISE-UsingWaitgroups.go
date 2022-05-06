a9197297f688e849c6ca40d781abdfddbe40c92e
Simardeep Singh <simardeepsngh1@gmail.com>
2022-05-06 10:00:35 +0000

    Exercise-1 Done

--- 6-Waitgroups/EXERCISE-UsingWaitgroups.go
+++ 6-Waitgroups/EXERCISE-UsingWaitgroups.go
@@ -17,25 +17,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
 
+	wg.Add(2)
 	start := time.Now()
 	doSomething()
 	doSomethingElse()
+	wg.Wait()
 	fmt.Println("I guess I'm done")
 	elapsed := time.Since(start)
 	fmt.Printf("Processes took %s", elapsed)
 }
 
 func doSomething() {
-	time.Sleep(time.Millisecond * 1500)
+	time.Sleep(time.Millisecond * 1000)
 	fmt.Println("\nI've done something")
+	wg.Done()
 }
 
 func doSomethingElse() {
-	time.Sleep(time.Millisecond * 1500)
+	time.Sleep(time.Millisecond * 1000)
 	fmt.Println("I've done something else")
+	wg.Done()
 }
