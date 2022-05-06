97759fcc9fd14a7a6107f58445ecbc1df0087eea
Simardeep Singh <simardeepsngh1@gmail.com>
2022-05-06 12:07:29 +0000

    Exercise-2 Done

--- 7-Channels/4-bufferedChannels.go
+++ 7-Channels/4-bufferedChannels.go
@@ -9,21 +9,19 @@ import (
 
 func main() {
 
-	c := make(chan string, 3)  // channel doesn't block until full ("buffered" channel)
+	c := make(chan string, 3) // channel doesn't block until full ("buffered" channel)
 	c <- "Hello "
 	c <- "Earth "
 	c <- "from Mars"
-	//c <- "from Venus"
+	// c <- "from Venus"
 
 	msg := <-c
 	fmt.Print(msg)
 
-	msg = <-c  // Notice we used = NOT := because msg is already declared
+	msg = <-c // Notice we used = NOT := because msg is already declared
 	fmt.Print(msg)
 
-	msg = <-c  // Notice we used = NOT := because msg is already declared
+	msg = <-c // Notice we used = NOT := because msg is already declared
 	fmt.Println(msg)
 
 }
-
-
